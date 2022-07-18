5ea55e856285069d3e5b60a5cab64fe27c25f111
mateuszsoszynski <soszynski94@googlemail.com>
2022-07-18 14:03:45 +0100

    navbar

---
+++ my-app/src/components/Navbar/Navbar.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  
+  return (
+    <>
+        <nav className='navbar'>
+        <div className='navbar-container'>
+            <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? "fa-solid fa-xmark" : 'fa-solid fa-bars'} />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                  Services
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                  Products
+                </Link>
+              </li>
+            </ul>
+        </div>
+    </nav>
+    </>
+  )
+}
+
+export default Navbar;
